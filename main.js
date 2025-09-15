e71ad7fd49bcd6ca44ddca18a1683d6319784a61
Hemanth-afk-dev <2400030277@kluniversity.in>
2025-09-15 15:00:10 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+let dog={
+    "name":"cherry",  weight:2.6,
+    color:"black"
+
+}
+console.log(typeof(dog))
+ console.log(dog)
+ dog.height=2
+console.log(dog)
+// dog.name="sunny"
+// console.log(dog)
+// Object.freeze(dog)
+// dog.name="Bunny"
+// console.log(dog)
+// console.log(dog.name)
+// console.log(dog["color"])
+// console.log(dog.pet name)
+// console.log(dog["pet name"])
+// let color1=dog.color
+// console.log(color1)
+// let {color:color2}=dog //let color2=dog.color
+// console.log(color2)
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+const myCar1 = new Car("jaguar", 2014);
+// const myCar2= new Car("BMW",2017)
+// const myCar3=new Car("KIA",2020)
+// console.log(myCar3)
+console.log(typeof(myCar1))
+// class Dog{
+//     constructor(name,color,height){
+//         this.name=name
+//         this.color=color
+//         this.height=height
+//     }
+// }
+// const dog1=new Dog("johnny","blue",2.3)
+// const dog2=new Dog("tom","Yellow",2.2) 
+// const dog3=new Dog("jerry","Red",2.4)
+// console.log(dog1)
+// console.log(typeof(dog1))
+// console.log(dog2)
+// console.log(typeof(dog2))
+// console.log(dog3)
+// console.log(typeof(dog3))
